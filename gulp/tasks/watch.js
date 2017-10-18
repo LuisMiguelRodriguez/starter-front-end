3a01dd12997dfae74c792e6124c4f59d56ee3afb
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-10-18 09:56:30 -0700

    Starter Template with Gulp - browserSync

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,37 @@
+var gulp        = require('gulp'),
+    watch       = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+
+
+gulp.task('watch', function(){
+
+  // initializes browserSync
+  // and shows it where the index.html is
+  browserSync.init({
+    server: {
+      notify: false,
+      baseDir: "public"
+    }
+  })
+
+  //Reloads the page  when index.html is changed
+  watch('./public/index.html', function(){
+    browserSync.reload();
+  });
+
+  //Looks for any changes for css files in the modules folder
+  // and then runs the cssInject task
+  watch('./styles/modules/**/*.css', function(){
+    gulp.start('cssInject');
+  });
+
+});
+
+
+// styles is add as a dependency it will run first
+// before cssInject runs
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./public/css/styles.css')
+    .pipe(browserSync.stream());
+});
