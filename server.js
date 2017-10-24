8b0b21188eae9b5fd8154d90c2d59700023aed2a
wesdoyle <wesdoyle08@gmail.com>
2017-10-24 14:05:25 -0700

    located all items to scrape required site

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // Dependencies
-var express = require("express");
-var exphbs = require("express-handlebars");
+var express    = require("express"),
+    exphbs     = require("express-handlebars"),
+    mongoose   = require('mongoose'),
+    bodyParser = require('body-parser'),
+    cheerio    = require('cheerio'),
+    request    = require('request');
 
 // Create an instance of the express app.
 var app = express();
@@ -23,6 +27,41 @@ var lunches = [
 
 // Routes
 app.get("/", function(req, res) {
+
+  request("https://jslive.com/", function(err, response, body){
+    if (err) throw err;
+
+    var $ = cheerio.load(body);
+
+    console.log('-------Length of Articles-------');
+
+    var articles = [];
+
+    // var length = $('#articles ')
+    // .find('.col-md-18')
+    // .each(function(i, element){
+    //   console.log($(this).children("h3").text());
+    //   console.log($(this).children("a").attr("href"));
+    //   console.log($(this).html());
+    //   console.log('#########################');
+    // })
+
+    // console.log(articles.length);
+
+    // console.log(length);
+
+    // console.log(body);
+    var length = $('article .body').each(function(i, element){
+
+      console.log($(this).children('h1').text());
+      console.log($(this).children('h1').find("a").attr('href'));
+      console.log($(this).children('p').text());
+
+      console.log('##############################');
+    });
+
+    // console.log( length);
+  })
   res.render("index");
 });
 
